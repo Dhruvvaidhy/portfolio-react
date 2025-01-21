725cadc55169ad593093841c1ddcef0484a3fb4d
Dhruvvaidhy <dhruv786.oceanmtech@gmail.com>
2025-01-21 12:39:19 +0530

    link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import Wave from "../../img/wave.png";
 import Insta from "@iconscout/react-unicons/icons/uil-instagram";
 import { UilLinkedin } from '@iconscout/react-unicons'
 // import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
-import Gitub from "@iconscout/react-unicons/icons/uil-github";
+import Gitub from "@iconscout/react-unicons/icons/uil-github";  
 
 const Footer = () => {
   return (
@@ -13,10 +13,16 @@ const Footer = () => {
       <div className="f-content">
         <span>dhruv786.oceanmtech@gmail.com</span>
         <div className="f-icons">
+        <a href="https://www.instagram.com/_dhulu_786?igsh=YWJ1eHpmdmNkdThh" target="_blank" >
           <Insta color="white" size={"3rem"} />
+          </a>
+          <a href="https://www.linkedin.com/in/dhruv-vaidhy-4230552b7/" target="_blank" >
           <UilLinkedin color="white" size={"3rem"} />
+          </a>
+          <a href="https://github.com/Dhruvvaidhy" target="_blank" >
           <Gitub color="white" size={"3rem"} />
-        </div>
+          </a>
+        </div> 
       </div>
     </div>
   );
